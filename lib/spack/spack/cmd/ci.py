9d65b1125622ece98ceabacb6ba9f3fa141c980e
Harmen Stoppels <me+bot@harmenstoppels.nl>
2026-09-24 18:04:18 +0200

    WIP 37 b

--- lib/spack/spack/cmd/ci.py
+++ lib/spack/spack/cmd/ci.py
@@ -272,9 +272,7 @@ def ci_reindex(args, ctx):
     remote_mirror_url = mirror_urls[0]
     mirror = spack.mirrors.mirror.Mirror(remote_mirror_url)
 
-    buildcache.update_index(
-        mirror, ctx.config, ctx.network, update_keys=True, repo_provider=ctx.repo_provider
-    )
+    buildcache.update_index(mirror, ctx, update_keys=True)
 
 
 def ci_rebuild(args, ctx):
